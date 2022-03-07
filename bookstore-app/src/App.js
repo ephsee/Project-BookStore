3dfcf3f949c82c19881514047802d5a6aed400f5
Francois Spies <fcspies@gmail.com>
2022-03-07 10:47:39 -0500

    cleaned up

---
+++ bookstore-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App App-header">
+      <div>I AM A BOOK STORE</div>
+      <p>this is where my stuff goes</p>
+
+    </div>
+  )
+}
+
+export default App;
