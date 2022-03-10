5b3cb6bb1eb257c6bfaa8e71ddec5a1d499d3ac4
Dalton <slattery.da@gmail.com>
2022-03-10 09:49:50 -0600

    select customer to buy

--- bookstore-app/src/components/Cart.js
+++ bookstore-app/src/components/Cart.js
@@ -1,24 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Dropdown, Button} from 'semantic-ui-react'
 
 function Cart({cart, customers, setCart}) {
-
-  const custId = customers.map( c => c.id )
-
+  const [userID, setUserID] = useState(1)
   const bookId = Object.values(cart).map( b => b.id )
-
   const showCart = Object.values(cart).map( o => <li key={o.id}> "{o.title}" by: {o.author}</li>)
 
-  function handleClick(arr) {
+  const users = customers.map( o => {
+    return {
+      key: o.id,
+      text: o.name,
+      value: o.id
+    }
+  })
 
-    arr.forEach( (e, i) => {
-      
+  const userOptions = customers.map( o => {
+    return <option key={o.id} value = {o.id}>{o.name}</option>
+  })
+
+  const handleChange = (e) => {
+    setUserID(e.target.value)
+  }
+
+  function handleClick(arr) {
+      arr.forEach( (e, i) => {
       const newOrder = {
-        customer_id: custId[1],
+        customer_id: userID,
         book_id: bookId[i]
       }
-
-      console.log(newOrder)
-
       fetch('http://localhost:9293/orders', {
         method: 'POST',
         headers: {
@@ -29,22 +38,18 @@ function Cart({cart, customers, setCart}) {
         .then(r=>r.json())
         .then(data => console.log('posted', data))
     })
-
     setCart({})
-
   }
-
 
   return (
     <div>Cart
-
       <ul>
-
         <div className="about">{showCart}</div>
-
       </ul>
-      <button onClick={()=> handleClick(showCart)}>BUY</button>
-
+      <select onChange = { (e) => handleChange(e)}>
+        {userOptions}
+      </select>
+      <button onClick={() => handleClick(showCart)}>Checkout</button>
     </div>
   )
 }
